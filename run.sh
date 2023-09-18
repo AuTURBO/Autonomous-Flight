5889ecc8650acdb13107f5764fa298f0273cdc9a
harderthan <kheo1772@gmail.com>
2023-09-18 22:09:15 +0900

    bring third-party and add LICENSE, run.sh

---
+++ run.sh
@@ -0,0 +1,6 @@
+# TODO(harderthan): 런치 파일들을 작성하고, 아래 코드가 동작하도록 만들어야 합니다. 
+# roslaunch plan_manage zhangjiajie.launch & sleep 1;
+# roslaunch traj_server traj_server.launch & sleep 1;
+# roslaunch Ctrl ctrl_md.launch  & sleep 1;
+# roslaunch plan_manage visualize.launch;
+wait
