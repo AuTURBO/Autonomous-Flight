97579117d7d4c7c52ed8374d5442a2b7d76097e1
harderthan <kheo1772@gmail.com>
2023-10-09 16:26:45 +0900

    add msgs and services

--- airsim_ros_pkgs/include/airsim_ros_pkgs/airsim_ros_wrapper.hpp
+++ airsim_ros_pkgs/include/airsim_ros_pkgs/airsim_ros_wrapper.hpp
@@ -1,4 +1,24 @@
 #ifndef AIRSIM_ROS_PKGS_INCLUDE_AIRSIM_ROS_PKGS_AIRSIM_ROS_WRAPPER_HPP_
 #define AIRSIM_ROS_PKGS_INCLUDE_AIRSIM_ROS_PKGS_AIRSIM_ROS_WRAPPER_HPP_
 
+#include "ros/ros.h"
+
+// ROS custom msgs.
+#include "airsim_ros_pkgs/Altimeter.h"
+#include "airsim_ros_pkgs/CarControls.h"
+#include "airsim_ros_pkgs/CarState.h"
+#include "airsim_ros_pkgs/Environment.h"
+#include "airsim_ros_pkgs/GPSYaw.h"
+#include "airsim_ros_pkgs/GimbalAngleEulerCmd.h"
+#include "airsim_ros_pkgs/GimbalAngleQuatCmd.h"
+#include "airsim_ros_pkgs/VelCmd.h"
+#include "airsim_ros_pkgs/VelCmdGroup.h"
+
+// ROS custom srvs.
+#include "airsim_ros_pkgs/Land.h"
+#include "airsim_ros_pkgs/LandGroup.h"
+#include "airsim_ros_pkgs/Reset.h"
+#include "airsim_ros_pkgs/Takeoff.h"
+#include "airsim_ros_pkgs/TakeoffGroup.h"
+
 #endif  // AIRSIM_ROS_PKGS_INCLUDE_AIRSIM_ROS_PKGS_AIRSIM_ROS_WRAPPER_HPP_
