84c5909e08dad507b5642db0f3b36037f1b592c5
Nicky Kim <kheo1772@gmail.com>
2023-10-17 23:42:33 +0900

    implement airsim_setings_parser

--- airsim_ros_pkgs/include/airsim_ros_pkgs/airsim_settings_parser.hpp
+++ airsim_ros_pkgs/include/airsim_ros_pkgs/airsim_settings_parser.hpp
@@ -1,4 +1,6 @@
-#include <chrono>
+#ifndef AIRSIM_ROS_PKGS_INCLUDE_AIRSIM_ROS_PKGS_AIRSIM_SETTINGS_PARSER_HPP_
+#define AIRSIM_ROS_PKGS_INCLUDE_AIRSIM_ROS_PKGS_AIRSIM_SETTINGS_PARSER_HPP_
+
 #include <iostream>
 
 // Airsim library headers
@@ -13,3 +15,31 @@ STRICT_MODE_OFF
 #endif  // !RPCLIB_MSGPACK
 #include "rpc/rpc_error.h"
 STRICT_MODE_ON
+
+namespace airsim_ros {
+namespace airsim_settings_parser {
+
+class AirSimSettingsParser {
+ public:
+  using AirSimSettings = msr::airlib::AirSimSettings;
+  using VehicleSetting = msr::airlib::AirSimSettings::VehicleSetting;
+
+  AirSimSettingsParser();
+
+  bool success() const;
+
+ private:
+  bool initializeSettings();
+  std::string getSimMode() const;
+  bool readSettingsTextFromFile(const std::string& settingsFilepath,
+                                std::string& settingsText);
+  bool getSettingsText(std::string& settingsText);
+
+  bool success_;
+  std::string settingsText_;
+};
+
+}  // namespace airsim_settings_parser
+}  // namespace airsim_ros
+
+#endif  // AIRSIM_ROS_PKGS_INCLUDE_AIRSIM_ROS_PKGS_AIRSIM_SETTINGS_PARSER_HPP_
--- airsim_ros_pkgs/src/airsim_settings_parser.cpp
+++ airsim_ros_pkgs/src/airsim_settings_parser.cpp
@@ -1,1 +1,61 @@
 #include "airsim_ros_pkgs/airsim_settings_parser.hpp"
+
+#include <iostream>
+
+namespace airsim_ros {
+namespace airsim_settings_parser {
+
+AirSimSettingsParser::AirSimSettingsParser() {
+  success_ = initializeSettings();
+}
+
+// mimics void ASimHUD::initializeSettings()
+bool AirSimSettingsParser::initializeSettings() {
+  if (!getSettingsText(settingsText_)) {
+    std::cerr << "failed to get settings text" << std::endl;
+    return false;
+  }
+
+  AirSimSettings::initializeSettings(settingsText_);
+
+  // not sure where settings_json initialized in
+  // AirSimSettings::initializeSettings() is actually used
+  Settings& settings_json = Settings::loadJSonString(settingsText_);
+  std::cout << "simmode_name: " << settings_json.getString("SimMode", "")
+            << "\n";
+
+  AirSimSettings::singleton().load(
+      std::bind(&AirSimSettingsParser::getSimMode, this));
+
+  return true;
+}
+
+bool AirSimSettingsParser::success() const { return success_; }
+
+std::string AirSimSettingsParser::getSimMode() const {
+  return Settings::loadJSonString(settingsText_).getString("SimMode", "");
+}
+
+bool AirSimSettingsParser::readSettingsTextFromFile(
+    const std::string& settingsFilepath, std::string& settingsText) {
+  auto ifs = std::ifstream(settingsFilepath);
+  if (ifs.good()) {
+    std::stringstream buffer;
+    buffer << ifs.rdbuf();
+    // todo airsim's simhud.cpp does error checking here
+    settingsText = buffer.str();  // todo convert to utf8 as done in simhud.cpp?
+    return true;
+  }
+  return false;
+}
+
+bool AirSimSettingsParser::getSettingsText(std::string& settingsText) {
+  bool success = readSettingsTextFromFile(
+      msr::airlib::Settings::Settings::getUserDirectoryFullPath(
+          "settings.json"),
+      settingsText);
+  return success;
+}
+
+}  // namespace airsim_settings_parser
+}  // namespace airsim_ros
